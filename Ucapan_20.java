1d210d74a6ffa4ef0775b2922a0cd9d28d3f9ef2
Icamdaf <icampoltek@gmail.com>
2023-11-23 08:33:43 +0700

    ucapan

---
+++ Ucapan_20.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Ucapan_20 {
+    public static String PenerimaUcapan(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Tuliskan nama orang yang akan diberi ucaopan:");
+        String namaOrang = sc.nextLine();
+        sc.close();
+        return namaOrang;
+    }
+
+    public static void main(String[] args) {
+        String nama = PenerimaUcapan();
+        System.out.println("Thank you" +nama+ "\n may the force be with you");
+        
+    }
+}

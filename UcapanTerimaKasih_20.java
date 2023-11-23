c4863762214c41a7a7de97a6310f84e4969b07c1
Icamdaf <icampoltek@gmail.com>
2023-11-23 08:52:55 +0700

    percobaan3.2

--- UcapanTerimaKasih_20.java
+++ UcapanTerimaKasih_20.java
@@ -1,16 +1,26 @@
 import java.util.Scanner;
 
 public class UcapanTerimaKasih_20 {
-public static String PenerimaUcapan(){
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Tuliskan nama yang akan diberi ucapan = ");
-    String namaOrang = sc.nextLine();
-    sc.close();
-    return namaOrang;
-}
-    public static void UcapanTerimaKasih(){
+    public static String PenerimaUcapan() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Tuliskan nama yang akan diberi ucapan = ");
+        String namaOrang = sc.nextLine();
+        sc.close();
+        return namaOrang;
+    }
+
+    public static void UcapanTerimaKasih() {
         String nama = PenerimaUcapan();
-        System.out.println("thank you" +nama+ "for being th best teacher in the world.\n" + "you inspired in me a love for learning and made me feel like i could ask you anything.");
+        System.out.println("Thank you " + nama + " for being the best teacher in the world.\n"
+                + "You inspired in me a love for learning and made me feel like I could ask you anything.");
+
+        
+        UcapanTambahan("youre the best, " + nama + "!");
+    }
+
+    
+    public static void UcapanTambahan(String tambahan) {
+        System.out.println("Ucapan Tambahan: " + tambahan);
     }
 
     public static void main(String[] args) {
